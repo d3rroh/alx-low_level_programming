ac610024a0df3d502e9d3de89623ccbfcb4cb6ff
d3rroh <micaslounge@gmail.com>
2022-03-09 22:19:18 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
 *main -indicates the start of the code
 *
 *
-*
+*Return - returns zero on success
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
