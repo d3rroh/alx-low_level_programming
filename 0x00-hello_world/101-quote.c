65787d0f6413c1fc7592455df536c0e0ee66b9b1
d3rroh <micaslounge@gmail.com>
2022-03-09 23:49:12 -0800

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main -indicates the start of the code
+*
+*
+* Return: returns zero on success
+*/
+
+int main(void)
+{
+	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
