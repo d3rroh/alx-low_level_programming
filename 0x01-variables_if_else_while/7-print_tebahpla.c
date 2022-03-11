6bfe268e26beb86329693be3d456433a6f717994
d3rroh <micaslounge@gmail.com>
2022-03-11 01:53:50 -0800

    dff

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-        char letter = 'z';
+	char letter = 'z';
 
         while (letter >= 'a')
-        {
-                putchar(letter);
-                letter--;
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
 }
