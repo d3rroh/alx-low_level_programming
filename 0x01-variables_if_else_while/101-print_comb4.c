ff9636f3f2de9ccb5711f11a561b3edda61c5cee
d3rroh <micaslounge@gmail.com>
2022-03-11 02:43:07 -0800

    sdfhj

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main -indicates the start of the code
+*
+*
+* Return: returns zero on success
+*/
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (k = 48; k <= 57; k++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				if ((i < k) && (k < j))
+				{
+					putchar(i);
+					putchar(k);
+					putchar(j);
+					if ((j < '9') | (i < '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
