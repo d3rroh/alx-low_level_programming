df925e54dbcc3d7ba68a449209f180d08252c901
d3rroh <micaslounge@gmail.com>
2022-03-11 01:23:59 -0800

    one

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main -indicates the start of the code
+*
+*
+* Return: returns zero on success
+*/
+
+int main(void)
+{
+	int num=0;
+	while(num<10)
+	{
+		printf("%d",num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
