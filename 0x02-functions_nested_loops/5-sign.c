efa9088373a718c7e91c15c6e099e80dc92863dc
d3rroh <micaslounge@gmail.com>
2022-03-15 02:51:04 -0700

    gd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*
+* print_sign - returns if its lower or not
+* Return: returns zero according to answer
+*/
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
