6c3a9cbe799c837bcb8519e53cfffbe431595f26
d3rroh <micaslounge@gmail.com>
2022-03-15 04:16:13 -0700

    hgxy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two ints
+ * @a: first int to add
+ * @b: second int to add
+ * Return: sum of both ints
+ **/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
