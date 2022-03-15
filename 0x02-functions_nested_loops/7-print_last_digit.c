7d14d246cac4dfbe0fa19e579231b3167077fd7b
d3rroh <micaslounge@gmail.com>
2022-03-15 03:16:25 -0700

    dsjh

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: number to compute last digit for
+ * Return: last digit
+ **/
+
+
+int print_last_digit(int num)
+{
+	num=num % 10;
+
+	if (num < 1)
+		num =- num;
+	_putchar(num + '0');
+	return (num);
+}
