d1b9853bcc14154a4b2b043c6e8b2a6e730d4cab
d3rroh <micaslounge@gmail.com>
2022-03-15 02:11:48 -0700

    der

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Prints Holberton
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints Holberton w/out stdio
+ *
+ * Return: 0 for success
+*/
+
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
+}
