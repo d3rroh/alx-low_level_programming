1d9285a9adb806471fec36dad850d3e0f336e141
d3rroh <micaslounge@gmail.com>
2022-03-15 03:02:11 -0700

    gh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* @n: int to compute as absolute
+* _abs - returns if its lower or not
+* Return: returns zero according to answer
+*/
+
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
