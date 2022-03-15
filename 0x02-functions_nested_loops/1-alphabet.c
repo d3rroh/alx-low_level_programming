1b33690b81bc61684a36ef610e35a2e9b76c0c59
d3rroh <micaslounge@gmail.com>
2022-03-15 02:18:18 -0700

    shdaj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*
+* print_alphabet - prints lower case alphabets
+*
+*/
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
