cac53f7620cc05588a19fb753af9ae8c129b6802
d3rroh <micaslounge@gmail.com>
2022-03-17 06:43:41 -0700

    fg

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,4 @@
 int positive_or_negative(int n);
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c)
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
