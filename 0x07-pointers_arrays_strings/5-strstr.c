90cbfb3ad1ba275763a87d38e924846b4b71dbf4
d3rroh <micaslounge@gmail.com>
2022-03-28 04:08:13 -0700

    jh

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,38 +1,50 @@
 #include "main.h"
+#define NULL 0
+
+/**
+ * compare - compares two strings
+ * @haystack: source string
+ * @needle: string being compared
+ * Description: checks if needle is at start of haystack
+ * Return: 1 if matches, 0 if not
+ **/
+
+int compare(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		if (haystack[i] == '\0')
+			return (0);
+		if (haystack[i] != needle[i])
+			return (0);
+	}
+	return (1);
+}
 
 /**
  * _strstr - locates a substring
- * @haystack: string to search through
- * @needle: string to match
- * Return: pointer to initial segment of haystack
- */
+ * @haystack: source string
+ * @needle: substring
+ * Description: finds first instance of needle in haystack
+ * Return: pointer to first instance, NULL if not in haystack
+ **/
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, start;
-	int nlen = 0;
+	int j;
 
-	i = 0;
-	j = 0;
-	start = 0;
+	if (needle[0] == '\0')
+		return (haystack);
 
-	while (needle[nlen] != '\0')
+	for (j = 0; haystack[j] != '\0'; j++)
 	{
-		nlen++;
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
+		if (needle[0] == haystack[j])
 		{
-			if (j == 0)
-			{
-				start = i;
-			}
-			if (j == nlen - 1)
-			{
-				return (haystack + start);
-			}
+			if (compare(&haystack[j], needle) == 1)
+				return (&haystack[j]);
 		}
 	}
-	return (0);
+	return (NULL);
 }
