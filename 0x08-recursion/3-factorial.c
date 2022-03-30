754281f3df5b648d79d2e581e5b9508f5cf96470
d3rroh <micaslounge@gmail.com>
2022-03-29 23:53:48 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - prints a string in reverse
+ * @n: given number
+ * Return: pointer
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+		return (-1);
+	return (n * factorial ( n -1));
+}
