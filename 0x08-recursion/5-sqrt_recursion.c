14c8af9e6f86b3e5f7681662aa4a0774098bdd49
d3rroh <micaslounge@gmail.com>
2022-03-30 00:21:18 -0700

    gfxy

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * prime_con - determine if n is a prime
- * @n: test case
- * @i: recurring factors
- * Description: determines if n is a prime using 2 parameters
- * Return: 1 if n is a prime, 0 if not
+ * sqr_con - finds square root of n
+ * @n: number to square
+ * @i: test case for root
+ * Description: find square root of n using recursion and two variables
+ * Return: square root of n or -1 if none
  **/
 
-int prime_con(int n, int i)
+int sqr_con(int n, int i)
 {
-	if (n % i == 0)
-		return (0);
+	if (n == i * i)
+		return (i);
 
-	else if (n >= (i + 1) * (i * 1))
-		return (prime_con(n, i + 1));
+	else if (n < i * i)
+		return (-1);
 
-	return (1);
+	return (sqr_con(n, i + 1));
 }
 
 /**
- * is_prime_number - determine if n is a prime
- * @n: number to check
- * Description: determine if n is a prime
- * Return: 1 if prime, 0 if not
+ * _sqrt_recursion - find natural square root of n
+ * @n: number to square
+ * Description: find square root of n using recursion
+ * Return: square root of n or -1 if none
  **/
 
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 {
-	if (n < 2)
-		return (0);
+	if (n < 0)
+		return (-1);
 
-	return (prime_con(n, 2));
+	return (sqr_con(n, 0));
 }
