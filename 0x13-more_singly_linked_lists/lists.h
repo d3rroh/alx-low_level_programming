c780bcdbc739772639b19577bbcda8be7910847f
d3rroh <micaslounge@gmail.com>
2022-04-25 20:37:56 +0300

    print

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,7 +19,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 /*Prototypes*/
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -35,6 +34,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 #endif
 
